6bf1d883bf7a187cd9047d9a66d84abb2fef4506
IrvinCaesar <152835521+IrvinCaesar@users.noreply.github.com>
2025-09-15 20:57:03 +0700

    Test Select Image + Tambah Data di collection
    frontend nnti berubah sesuai banyak Produk/Category

--- janAgro/src/components/pages/Profile.jsx
+++ janAgro/src/components/pages/Profile.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
 import './Profile.css';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 const Profile = () => {
   const [data, setData] = useState([]);
+  const [selectedImage, setSelectedImage] = useState(null);
+  
+
+
+
+ useEffect(() => {
+    localStorage.setItem("a",selectedImage)
+ },[selectedImage])
+  
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,11 +27,46 @@ const Profile = () => {
     fetchData();
   }, []);
 
+
+
+
+
+
   return (
     <div>
       <h1><center>GORONG DIBUAT COKKK</center></h1>
       <img className="hans"src="/src/image/han.jpg" alt="" />
       <img src="/src/image/download.jpg" alt="" />
+    <br/>
+
+      {selectedImage && (
+        
+        <div>
+          <h2>The Image:</h2>
+          {/* Display the selected image */}
+          <img
+            alt="not found"
+            width={"250px"}
+            src={URL.createObjectURL(selectedImage)}
+          />
+          <br /> <br />
+          {/* Button to remove the selected image */}
+          <button onClick={() => setSelectedImage(null)}>Remove</button>
+        </div>
+      )}
+
+      <br />
+      <h2>Pilih Gambar</h2>
+     <input
+        type="file"
+        name="myImage"
+        // Event handler to capture file selection and update the state
+        onChange={(event) => {
+          console.log(event.target.files[0]); // Log the selected file
+          setSelectedImage(event.target.files[0]); // Update the state with the selected file
+        }}
+      />
+
 
       <div>
         <h1>List of Items</h1>
@@ -41,7 +85,7 @@ const Profile = () => {
               
             ))
           ) : (
-            <p>No data to display TOLOL.</p>
+            <p>No data to display.</p>
           )}
         </ul>
       </div>
